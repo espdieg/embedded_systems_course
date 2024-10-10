a2a73f6ce9d2ee2c03a9696c5def81f79ebc3394
Diego Espinoza <espdieg@outlook.com>
2024-10-09 22:37:46 -0700

    Create main.c

---
+++ lab2/main.c
@@ -0,0 +1,10 @@
+#include "lab2.h"                          
+
+void main(void) {
+  // here we just uncomment each line as we progress through the tasks of this lab
+  
+  task_2();
+  //task_3();
+  //task_4();
+  //task_5();
+}
